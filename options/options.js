084ef2fd2be5d7964c95281f7c910f1e9b20fafc
django <django@e59df45c5270>
2024-06-20 20:57:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-frost-391",
+  url: "https://late-frost-391.botics.co",
+  api: "https://late-frost-391.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
